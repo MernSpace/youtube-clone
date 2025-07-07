626bdda771340eea61e4fd747e4980229e93f2d4
mernspace <sifat355y@.com>
2025-07-07 09:24:13 +0600

    video upload modal done with db recod

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { pgTable, text, timestamp, uniqueIndex, uuid } from 'drizzle-orm/pg-core'
 import { relations } from 'drizzle-orm'
+import { te } from 'date-fns/locale'
 
 
 export const users = pgTable("users", {
@@ -37,6 +38,12 @@ export const videos = pgTable("videos", {
     id: uuid("id").primaryKey().defaultRandom(),
     title: text("title").notNull(),
     description: text("description"),
+    muxStatus: text("mux_status"),
+    muxAssetId: text("mux_asset_id").unique(),
+    muxUploadId: text("mux_upload_id").unique(),
+    muxPlaybackId: text("mux_playback_id").unique(),
+    muxTrackId: text("mux_track_id").unique(),
+    muxTrackStatus: text("mux_track_status"),
     userId: uuid("user_id").references(() => users.id, {
         onDelete: "cascade"
     }).notNull(),
